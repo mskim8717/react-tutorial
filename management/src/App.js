77e744a2f7b86345f6bbd176c9388130f5e90cc1
MinsooKim <ms.kim8717@gmail.com>
2020-07-30 01:04:19 +0900

    Add Customer class

--- management/src/App.js
+++ management/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customerInfo = {
+  'name': '홍길동',
+  'birthday':'20202020',
+  'gender':'남자',
+  'job':'직장인'
+}
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop manangement system!</h2>
-    </div>
+    <Customer 
+      name={customerInfo.name}
+      birthday={customerInfo.birthday}
+      gender={customerInfo.gender}
+      job={customerInfo.job}/>
   );
 }
 
