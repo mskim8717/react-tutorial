d58e0d1d99bc13fc6481adeddcff4f6ebaa3bc00
MinsooKim <ms.kim8717@gmail.com>
2020-08-02 17:44:20 +0900

    Customer file update

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -3,20 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
 
-const customerInfo = {
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any',
   'name': '홍길동',
-  'birthday':'20202020',
+  'birthday':'2020-20-20',
   'gender':'남자',
   'job':'직장인'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '김민수',
+  'birthday':'2020-11-20',
+  'gender':'남자',
+  'job':'직장인'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '김민영',
+  'birthday':'2020-11-20',
+  'gender':'여자',
+  'job':'자영업'
 }
+]
 
 function App() {
   return (
-    <Customer 
-      name={customerInfo.name}
-      birthday={customerInfo.birthday}
-      gender={customerInfo.gender}
-      job={customerInfo.job}/>
+    <div>
+      {
+      customers.map(c => {
+        return (
+          <Customer 
+          key={c.id}
+            id = {c.id}
+            image = {c.image}
+            name = {c.name}
+            birthday = {c.birthday}
+            gender = {c.gender}
+            job = {c.job}
+            />
+        );
+      })
+      }
+
+    </div>
   );
 }
 
